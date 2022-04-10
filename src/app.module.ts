8822b9ca1811d92890ca5f19d19bd1b1fc5c686b
Andrey Zhygarev <infernalsteel666@gmail.com>
2022-04-10 22:14:28 +0300

    created connection to postgres and added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 // Modules
 import { TagModule } from '@app/tag/tag.module';
 
 @Module({
-  imports: [TagModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'mediumclone',
+    }),
+    TagModule,
+  ],
   controllers: [],
   providers: [],
 })
