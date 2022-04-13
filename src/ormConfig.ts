2add16e273bfb3fd9b67733bf3bc711e32e81068
Andrey Zhygarev <infernalsteel666@gmail.com>
2022-04-13 22:03:25 +0300

    creted first migration

---
+++ src/ormConfig.ts
@@ -0,0 +1,18 @@
+import { ConnectionOptions } from 'typeorm';
+
+export const ormConfig = (): ConnectionOptions => ({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: 'mediumclone',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+});
+
+export default ormConfig();
